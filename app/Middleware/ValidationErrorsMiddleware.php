87b200225d43f830f4dd1093d7efa6a3a5f928b8
minz <gonzalezlozano.d@gmail.com>
2019-01-06 23:52:08 +0100

    15/29
    15/29

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -7,7 +7,7 @@
         public function __invoke($request, $response, $next)
         {
 
-            $this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+            $this->container->view->getEnvironment()->addGlobal('errors', @$_SESSION['errors']);
 
             unset($_SESSION['errors']);
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -63,9 +63,15 @@
         return new \App\Controllers\Auth\AuthController($container);
     };
 
+    $container['csrf'] = function($container){
+        return new \Slim\Csrf\Guard;
+    };
+
     $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
     $app->add(new \App\Middleware\OldInputMiddleware($container));
 
-    v::with('App\\Validation\\Rules\\');
+    $app->add($container->csrf);
+
+    v::with('App\\Validation\\Rules\\362');
 
     require __DIR__ . '/../app/routes.php';
