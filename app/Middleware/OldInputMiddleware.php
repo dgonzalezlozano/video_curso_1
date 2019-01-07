1808ea0de5f54505aa87a9befbeac7783149835e
minz <gonzalezlozano.d@gmail.com>
2019-01-07 22:46:18 +0100

    17/29
    17/29

--- app/Middleware/OldInputMiddleware.php
+++ app/Middleware/OldInputMiddleware.php
@@ -7,7 +7,7 @@
         public function __invoke($request, $response, $next)
         {
 
-            $this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+            $this->container->view->getEnvironment()->addGlobal('old', @$_SESSION['old']);
 
             $_SESSION['old'] = $request->getParams();
 
