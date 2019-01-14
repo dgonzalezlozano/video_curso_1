fcea25a9544b7743fcd077dd9ed51d7f4fec2b56
minz <gonzalezlozano.d@gmail.com>
2019-01-14 22:54:41 +0100

    29/29
    29/29 final

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,22 @@
 <?php
 
     use App\Middleware\AuthMiddleware;
+    use App\Middleware\GuestMiddleware;
 
     $app->get('/', function($request, $response){
         return $this->view->render($response, 'home.twig');
     });
 
+    $app->group('', function(){
+        $this->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
+        $this->post('/auth/signup', 'AuthController:postSignUp');
+
+        $this->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
+        $this->post('/auth/signin', 'AuthController:postSignIn');
+    })->add(new GuestMiddleware($container));
+
     $app->get('/home', 'HomeController:index')->setName('home');
 
-    $app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
-    $app->post('/auth/signup', 'AuthController:postSignUp');
-
-    $app->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
-    $app->post('/auth/signin', 'AuthController:postSignIn');
 
     $app->group('', function(){
         $this->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
